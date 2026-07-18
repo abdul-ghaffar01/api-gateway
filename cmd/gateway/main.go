632764c89d941c639638752f15d1e73047bd5a87
Abdul Ghaffar <agscontact777@gmail.com>
2026-07-18 15:30:43 +0500

    update name of folder

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"flag"
+	"os"
+	"github.com/abdul-ghaffar01/api-gateway/internal/config"
+)
+
 
 func main(){
-	
+	path := "config.yaml"
+
+	if env := os.Getenv("CONFIG_PATH"); env != "" {
+		path = env
+	}
+
+	configPath := flag.String("config", path, "Configuration file")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
 }
