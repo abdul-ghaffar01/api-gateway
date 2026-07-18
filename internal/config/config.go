4c2f8a25a8c9f01e63c9ec4ba95cf0ab4407fe3b
Abdul Ghaffar <agscontact777@gmail.com>
2026-07-18 17:18:39 +0500

    feat: validate req methods

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,15 +3,15 @@ package config
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
 
-
 // To validate the configurations
 func validateConfig(config Config) error {
 	// Checking if port is valid
-	if config.Server.Port < 0 || config.Server.Port > 65535 {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
 		return fmt.Errorf("Invalid port number passed")
 	}
 
@@ -25,18 +25,42 @@ func validateConfig(config Config) error {
 
 		// Checking if the base_url or path are passed
 		if route.Base_url == ""{
-			return fmt.Errorf("base_url is not defined in route %v", route.Name)
+			return fmt.Errorf("base_url is not defined in route \"%v\"", route.Name)
 		}
 
 		// Checking if the base_url or path are passed
 		if route.Path == "" {
-			return fmt.Errorf("path is not defined in route %v", route.Name)
+			return fmt.Errorf("path is not defined in route \"%v\"", route.Name)
+		}
+
+		// Checking for http methods
+		if len(route.Methods) == 0 {
+			return fmt.Errorf("method is not defined for route \"%v\"", route.Name)
+		}
+
+		// if all the passed methods are valid
+		for _, method := range route.Methods {
+			var VALID_METHODS = [10]string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "TRACE", "CONNECT", "QUERY"}
+			isValid := false
+
+			for _, v := range VALID_METHODS {
+				if strings.ToUpper(method) == v {
+					isValid = true
+					break
+				}
+			}
+
+			if !isValid {
+				return fmt.Errorf("Invalid method name \"%v\" in route \"%v\"", method, route.Name)
+			}
 		}
 	}
 
 	return nil
 }
 
+
+// To load the config file
 func Load(path string) (*Config, error) {
 	// Reading the config file
 	data, err := os.ReadFile(path)
@@ -53,6 +77,7 @@ func Load(path string) (*Config, error) {
 		return nil, err
 	}
 
+	// checking if the configuration is correct
 	if err := validateConfig(config); err != nil {
 		return nil, err
 	}
